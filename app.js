2ecb8a44410a90a082e3791ac58c38d44a685aca
Benjamin Yee <stmapman7@aol.com>
2019-10-31 20:18:30 -0700

    Add text analyzer

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// your code here!
+function getAverageWordLength(tokens){
+  var totalLength = tokens.join("").length;
+  return (totalLength / tokens.length).toFixed(2);
+}
+
+function countDistinctWords(tokens) {
+  var distinctWords = [];
+  for (var i=0; i<tokens.length; i++) {
+    if(distinctWords.indexOf(tokens[i]) === -1) {
+      distinctWords.push(tokens[i]);
+    }
+  }
+  return distinctWords.length;
+}
+
+function tokenizeText(text) {
+  return text.toLowerCase().match(/\b[^\s]+\b/g).sort();
+}
+
+function removeReturns(text) {
+  return text.replace(/\r?n|\r/g, "");
+}
+
+function reportOnText(text) {
+  var tokens = tokenizeText(text);
+  var numDistinctWords = countDistinctWords(tokens);
+  var numTotalWords = tokens.length;
+  var averageWordLength = getAverageWordLength(tokens);
+
+  var textReport = $('.js-text-report');
+  textReport.find('.js-word-count').text(numTotalWords);
+  textReport.find('.js-unique-word-count').text(numDistinctWords);
+  textReport.find('.js-average-word-length').text(averageWordLength + " characters");
+  textReport.removeClass('hidden');
+}
+function watchFormSubmission() {
+  $('.js-text-form').submit(function(event) {
+    event.preventDefault();
+    // get the text the user submitted
+    var userText = $(this).find('#user-text').val();
+    reportOnText(removeReturns(userText));
+  });
+}
+
+// equivalent to `$(document).ready(function() {...})`
+$(function() {
+  watchFormSubmission();
+});
